be5fa90f75f593ab5a0079bab67c8227c40e2b2e
Muhammet Enes AY <enesaym@gmail.com>
2024-02-04 22:33:21 +0300

    log ıgnore

---
+++ AdvanceApi.LOG/Log/ILog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceApi.LOG.Log
+{
+	public interface ILog
+	{
+		void TakeInfoLog(string message);
+		void TakeErrorLog(string message);
+	}
+}
---
+++ AdvanceApi.LOG/Log/Log.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceApi.LOG.Log
+{
+	public class Log : ILog
+	{
+		private readonly Logger _logger;
+        public Log()
+        {
+			_logger = LogManager.GetCurrentClassLogger();
+		}
+        public void TakeInfoLog(string message)
+		{
+			_logger.Info(message);
+		}
+		public void TakeErrorLog(string message)
+		{
+			_logger.Error(message);
+		}
+	}
+}
